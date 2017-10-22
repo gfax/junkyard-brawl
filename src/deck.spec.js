164356cc12a11d662a996e30b65d1611084df5a1
Jay Thomas <jay@gfax.ch>
2017-10-21 23:38:42 -0400

    Add "Meal Steal"

--- src/deck.spec.js
+++ src/deck.spec.js
@@ -1096,6 +1096,113 @@ Ava.test('Matress shouldn\'t have an effect when there was no damage', (t) => {
   t.is(game.discard.length, 3)
 })
 
+Ava.test('Meal Steal should steal multiple cards', (t) => {
+  const announceCallback = Sinon.spy()
+  const game = new Junkyard('player1', 'Jay', announceCallback)
+  game.addPlayer('player2', 'Kevin')
+  game.start()
+
+  const [player1, player2] = game.players
+  const mealSteal = Deck.getCard('meal-steal')
+  const gutPunch = Deck.getCard('gut-punch')
+  const soup = Deck.getCard('soup')
+  const sub = Deck.getCard('sub')
+  player1.hand.push(mealSteal)
+  player2.hand = [soup, sub, gutPunch, sub]
+
+  game.play(player1, mealSteal)
+
+  t.true(announceCallback.calledWith('card:meal-steal:steal-multiple'))
+  t.is(player1.hp, player1.maxHp)
+  t.is(player1.hand.length, player1.maxHand)
+  t.truthy(find(player2.hand, gutPunch))
+  t.is(game.turns, 1)
+  t.is(game.discard.length, 4)
+})
+
+Ava.test('Meal Steal should heal with multiple stolen cards', (t) => {
+  const announceCallback = Sinon.spy()
+  const game = new Junkyard('player1', 'Jay', announceCallback)
+  game.addPlayer('player2', 'Kevin')
+  game.start()
+
+  const [player1, player2] = game.players
+  const mealSteal = Deck.getCard('meal-steal')
+  const gutPunch = Deck.getCard('gut-punch')
+  const soup = Deck.getCard('soup')
+  const sub = Deck.getCard('sub')
+  player1.hp = 1
+  player1.hand.push(mealSteal)
+  player2.hand = [soup, sub, gutPunch, sub]
+
+  game.play(player1, mealSteal)
+
+  t.is(player1.hp, 6)
+})
+
+Ava.test('Meal Steal should steal one card', (t) => {
+  const announceCallback = Sinon.spy()
+  const game = new Junkyard('player1', 'Jay', announceCallback)
+  game.addPlayer('player2', 'Kevin')
+  game.start()
+
+  const [player1, player2] = game.players
+  const mealSteal = Deck.getCard('meal-steal')
+  const gutPunch = Deck.getCard('gut-punch')
+  const sub = Deck.getCard('sub')
+  player1.hand.push(mealSteal)
+  player2.hand = [gutPunch, sub]
+
+  game.play(player1, mealSteal)
+
+  t.true(announceCallback.calledWith('card:meal-steal:steal-one'))
+  t.is(player1.hp, player1.maxHp)
+  t.is(player1.hand.length, player1.maxHand)
+  t.truthy(find(player2.hand, gutPunch))
+  t.is(game.turns, 1)
+  t.is(game.discard.length, 2)
+})
+
+Ava.test('Meal Steal should steal zero cards', (t) => {
+  const announceCallback = Sinon.spy()
+  const game = new Junkyard('player1', 'Jay', announceCallback)
+  game.addPlayer('player2', 'Kevin')
+  game.start()
+
+  const [player1, player2] = game.players
+  const mealSteal = Deck.getCard('meal-steal')
+  const gutPunch = Deck.getCard('gut-punch')
+  player1.hand.push(mealSteal)
+  player2.hand = [gutPunch]
+
+  game.play(player1, mealSteal)
+
+  t.true(announceCallback.calledWith('card:meal-steal:steal-zero'))
+  t.is(player1.hp, player1.maxHp)
+  t.is(player1.hand.length, player1.maxHand)
+  t.truthy(find(player2.hand, gutPunch))
+  t.is(game.turns, 1)
+  t.is(game.discard.length, 1)
+})
+
+Ava.test('Meal Steal should have no effect with no stolen cards', (t) => {
+  const announceCallback = Sinon.spy()
+  const game = new Junkyard('player1', 'Jay', announceCallback)
+  game.addPlayer('player2', 'Kevin')
+  game.start()
+
+  const [player1, player2] = game.players
+  const mealSteal = Deck.getCard('meal-steal')
+  const gutPunch = Deck.getCard('gut-punch')
+  player1.hp = 1
+  player1.hand.push(mealSteal)
+  player2.hand = [gutPunch]
+
+  game.play(player1, mealSteal)
+
+  t.is(player1.hp, 1)
+})
+
 Ava.test('Mirror should duplicate attacks', (t) => {
   const announceCallback = Sinon.spy()
   const game = new Junkyard('player1', 'Jay', announceCallback)
